0f7c4cd3d52edd093330a0c323d9ee5ca5aaa836
ginalu <ginalu@utexas.edu>
2016-11-30 16:15:01 -0600

    header

--- ClientMain.java
+++ ClientMain.java
@@ -1,3 +1,15 @@
+/* ClientMain.java
+ * EE422C Project 7 submission by
+ * Gina Lu
+ * gbl286
+ * 16480
+ * Jessica Slaughter
+ * jts3329
+ * 16470
+ * Slip days used: <1>
+ * Fall 2016
+ */
+
 package assignment7;
 
 import java.io.*;
